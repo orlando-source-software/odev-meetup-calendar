5c5787360fa57a1a19e699c6a417074a69c265f9
Brian Gates <brian.gates@theironyard.com>
2016-12-02 23:03:52 -0500

    more logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
 import './index.css'
-
+console.log('reactdom', ReactDOM, '.render', ReactDOM.render, 'routes', routes, 'elm', document.getElementById('root'))
 ReactDOM.render(
   routes,
   document.getElementById('root')
